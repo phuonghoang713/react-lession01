b07064af626a8a73b3e9fafdfa46931f1a51a298
Hoang Nguyen <phuonghoang713@gmail.com>
2018-09-10 13:16:06 +0700

    add bootstraps

---
+++ src/Demo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello World! on demo</h2>
+      </div>
+      
+    );
+  }
+}
+
+export default Demo;

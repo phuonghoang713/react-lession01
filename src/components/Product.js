baa64f223faea7e6a29f768977273d06fc819bde
Hoang Nguyen <phuonghoang713@gmail.com>
2018-09-11 15:13:59 +0700

    no need add parameter because changing define type function = () => {}

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,6 +9,9 @@ class Product extends Component {
     alert("You do" + this.props.children)
     console.log("You do" + this.props.children)
   }
+  onClick2 = () => {
+    alert(this.props.children)
+  }
   render() {
     return (
       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
@@ -18,7 +21,7 @@ class Product extends Component {
               <h3> {this.props.children} </h3>
               <h3> {this.props.price} VND</h3> 
             <p>
-              <button type="button" className="btn btn-primary" onClick={ this.onClick}>Buy</button>
+              <button type="button" className="btn btn-primary" onClick={ this.onClick2}>Buy</button>
               <button type="button" className="btn btn-default" onClick={ this.onClick}>Add To Wishlist</button>
             </p>
           </div>

0a03bac588f9814746b5b465ce28bdcf633cf0bc
Hoang Nguyen <phuonghoang713@gmail.com>
2018-09-11 14:50:19 +0700

    handle event type 1 ()=> function

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 
 class Product extends Component {
+  onClick(param, param1){
+    alert("You do"+param + ";" + param1)
+    console.log("You do"+param + ";" + param1)
+  }
   render() {
     return (
       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <div className="thumbnail">
-          <img width={300} height={500} mode='fit' src={this.props.src} alt={this.props.children}></img>
+          <img width={200} height={200} mode='fit' src={this.props.src} alt={this.props.children}></img>
           <div className="caption">
+     
               <h3> {this.props.children} </h3>
-            <p>
+            
               <h3> {this.props.price}  VND</h3> 
-            </p>
+         
             <p>
-              <a href="#" className="btn btn-primary">Buy</a>
-              <a href="#" className="btn btn-default">Add To Wishlist</a>
+              <button type="button" className="btn btn-primary" onClick={() => this.onClick('Buy', this.props.children)}>Buy</button>
+              <button type="button" className="btn btn-default" onClick={() => this.onClick('Add To Wishlist', this.props.children)}>Add To Wishlist</button>
             </p>
           </div>
           

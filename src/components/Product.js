b5126a865e54da6fbafcbfd08091485a0365ac94
Hoang Nguyen <phuonghoang713@gmail.com>
2018-09-11 15:02:43 +0700

    no need add parameter because define in constructor

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class Product extends Component {
-  onClick(param, param1){
-    alert("You do"+param + ";" + param1)
-    console.log("You do"+param + ";" + param1)
+  constructor(props){
+    super(props);
+    this.onClick =  this.onClick.bind(this);
+  }
+  onClick(){
+    alert("You do" + this.props.children)
+    console.log("You do" + this.props.children)
   }
   render() {
     return (
@@ -11,14 +15,11 @@ class Product extends Component {
         <div className="thumbnail">
           <img width={200} height={200} mode='fit' src={this.props.src} alt={this.props.children}></img>
           <div className="caption">
-     
               <h3> {this.props.children} </h3>
-            
-              <h3> {this.props.price}  VND</h3> 
-         
+              <h3> {this.props.price} VND</h3> 
             <p>
-              <button type="button" className="btn btn-primary" onClick={() => this.onClick('Buy', this.props.children)}>Buy</button>
-              <button type="button" className="btn btn-default" onClick={() => this.onClick('Add To Wishlist', this.props.children)}>Add To Wishlist</button>
+              <button type="button" className="btn btn-primary" onClick={ this.onClick}>Buy</button>
+              <button type="button" className="btn btn-default" onClick={ this.onClick}>Add To Wishlist</button>
             </p>
           </div>
           
